7ac7e000e2850678eb54f08822f472f0171a534b
Mark Lewis <mlewis@trinity.edu>
2019-11-07 12:34:44 -0600

    Starting the classification.

---
+++ src/main/scala/sparkml/ClassifyAdults.scala
@@ -0,0 +1,45 @@
+package sparkml
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.feature.StringIndexer
+
+object ClassifyAdults {
+  def main(args: Array[String]): Unit = {
+    val spark =
+      SparkSession
+        .builder()
+        .master("local[*]")
+        .appName("Temp Data")
+        .getOrCreate()
+    import spark.implicits._
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    val rawData = spark.read.option("header", "true").option("inferSchema", "true").csv("data/adult.csv")
+
+    rawData.show()
+    rawData.printSchema()
+
+    val withNumIncome = rawData.withColumn("numIncome", when('income === ">50K", 1).otherwise(0))
+
+    val intFeatures = "age educationNum capitalGain capitalLoss hoursPerWeek".split(" ")
+    val stringFeatureCols = "workclass maritalStatus occupation relationship race sex".split(" ")
+    val indexedData = stringFeatureCols.foldLeft(withNumIncome) {(ds, name) =>
+      val indexer = new StringIndexer().setInputCol(name).setOutputCol(name+"-i")
+      indexer.fit(ds).transform(ds).drop("name")
+    }
+
+    val va = new VectorAssembler().setInputCols(intFeatures ++ stringFeatureCols.map(_+"-i")).setOutputCol("features")
+    val vectData = va.transform(indexedData)
+
+    val randomForest = new RandomForestClassifier().setLabelCol("numIncome")
+    val rfModel = randomForest.fit(vectData)
+
+    val classified = rfModel.transform(vectData)
+    classified.show()
+
+    spark.stop()
+  }
+}
--- src/main/scala/sparkml/TempMLStuff.scala
+++ src/main/scala/sparkml/TempMLStuff.scala
@@ -9,6 +9,10 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature.StandardScaler
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.clustering.KMeans
+import swiftvis2.plotting.styles.ScatterStyle
+import swiftvis2.plotting._
+import swiftvis2.plotting.renderer.SwingRenderer
 
 object TempMLStuff extends App {
   val spark =
@@ -43,42 +47,57 @@ object TempMLStuff extends App {
 
   val dataWithTime = data.withColumn("time", 'year + 'dayOfYear / 366.0)
 
+  // val va = new VectorAssembler()
+  //   .setInputCols(Array("time", "precip", "tave", "tmax", "tmin"))
+  //   .setOutputCol("features")
+  // // println(va.explainParams())
+  // val dataWithBigFeature = va.transform(dataWithTime)
+  // dataWithBigFeature.show(false)
+
+  // val scaler = new StandardScaler()
+  //   .setInputCol("features")
+  //   .setOutputCol("scaledFeatures")
+  // val scalerModel = scaler.fit(dataWithBigFeature)
+  // val dataScaled = scalerModel.transform(dataWithBigFeature)
+  // dataScaled.show()
+  // dataScaled.select('scaledFeatures).show(false)
+
+  // Find warming is SA
+  // val timeVA = new VectorAssembler().setInputCols(Array("time")).setOutputCol("timeVect")
+  // val dataWithTimeFeature = timeVA.transform(dataWithTime)
+
+  // val lr = new LinearRegression()
+  //   .setFeaturesCol("timeVect")
+  //   .setLabelCol("tave")
+
+  // val lrModel = lr.fit(dataWithTimeFeature)
+  // println(lrModel.coefficients)
+  // val fitData = lrModel.transform(dataWithTimeFeature)
+  // // fitData.show()
+
+  // // Find variation in San Antonio
+  // val dataWithSinCos = dataWithTime.withColumn("sin", sin('time*math.Pi*2)).withColumn("cos", cos('time*math.Pi*2))
+  // val sinVA = new VectorAssembler().setInputCols(Array("time", "sin", "cos")).setOutputCol("sinFeature")
+  // val dataWithSinFeature = sinVA.transform(dataWithSinCos)
+  // val sinLR = new LinearRegression().setFeaturesCol("sinFeature").setLabelCol("tave")
+  // val sinModel = sinLR.fit(dataWithSinFeature)
+  // println(sinModel.coefficients)
+
+  // Stuff for clustering
   val va = new VectorAssembler()
-    .setInputCols(Array("time", "precip", "tave", "tmax", "tmin"))
+    .setInputCols(Array("precip", "tave", "tmax", "tmin"))
     .setOutputCol("features")
-  // println(va.explainParams())
   val dataWithBigFeature = va.transform(dataWithTime)
   dataWithBigFeature.show(false)
 
-  val scaler = new StandardScaler()
-    .setInputCol("features")
-    .setOutputCol("scaledFeatures")
-  val scalerModel = scaler.fit(dataWithBigFeature)
-  val dataScaled = scalerModel.transform(dataWithBigFeature)
-  dataScaled.show()
-  dataScaled.select('scaledFeatures).show(false)
+  val kmeans = new KMeans().setK(4)
+  val kmeansModel = kmeans.fit(dataWithBigFeature)
+  val dataWithClusters1 = kmeansModel.transform(dataWithBigFeature)
+  dataWithClusters1.show()
 
-  // Find warming is SA
-  val timeVA = new VectorAssembler().setInputCols(Array("time")).setOutputCol("timeVect")
-  val dataWithTimeFeature = timeVA.transform(dataWithTime)
-
-  val lr = new LinearRegression()
-    .setFeaturesCol("timeVect")
-    .setLabelCol("tave")
-
-  val lrModel = lr.fit(dataWithTimeFeature)
-  println(lrModel.coefficients)
-  val fitData = lrModel.transform(dataWithTimeFeature)
-  // fitData.show()
-
-  // Find variation in San Antonio
-  val dataWithSinCos = dataWithTime.withColumn("sin", sin('time*math.Pi*2)).withColumn("cos", cos('time*math.Pi*2))
-  val sinVA = new VectorAssembler().setInputCols(Array("time", "sin", "cos")).setOutputCol("sinFeature")
-  val dataWithSinFeature = sinVA.transform(dataWithSinCos)
-  val sinLR = new LinearRegression().setFeaturesCol("sinFeature").setLabelCol("tave")
-  val sinModel = sinLR.fit(dataWithSinFeature)
-  println(sinModel.coefficients)
-
-
+  val pdata = dataWithClusters1.select('dayOfYear.as[Double], 'tave.as[Double], 'prediction.as[Double]).collect()
+  val cg = ColorGradient(0.0 -> RedARGB, 1.0 -> GreenARGB, 2.0 -> BlueARGB, 3.0 -> MagentaARGB)
+  val plot = Plot.simple(ScatterStyle(pdata.map(_._1), pdata.map(_._2), colors = cg(pdata.map(_._3))))
+  SwingRenderer(plot, 800, 800, true)
   spark.sparkContext.stop()
 }
